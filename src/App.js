9e7ed4fba10083b875d8917da424ed266a200a62
rick427 <rchard@yahoo.com>
2021-09-18 22:48:48 +0100

    finished gateway

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import ReactDOM from "react-dom";
 
-function App() {
+const PayPalButton = window.paypal.Buttons.driver("react", { React, ReactDOM });
+
+const App = () => {
+  //@: create order 
+  function _createOrder(data, actions) {
+    return actions.order.create({
+      purchase_units: [
+        {
+          amount: {
+            value: "1",
+          },
+        },
+      ],
+    });
+  }
+
+  //@: After a success payment
+  async function _onApprove(data, actions) {
+    let order = await actions.order.capture();
+    console.log(order);
+    return order;
+  }
+
+  //:@ If error
+  function _onError(err) {
+    console.log(err);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="card">
+        <PayPalButton
+          createOrder={(data, actions) => _createOrder(data, actions)}
+          onApprove={(data, actions) => _onApprove(data, actions)}
+          onCancel={() => _onError("Canceled")}
+          onError={(err) => _onError(err)}
+        />
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
